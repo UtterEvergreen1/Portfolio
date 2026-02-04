924b75bbaaf6118b40a00e5e3f60e410ffe669d6
Daniel <102389559+UtterEvergreen1@users.noreply.github.com>
2026-02-04 01:58:46 -0800

    Fixed spacing and project description.

--- assets/js/load_components.js
+++ assets/js/load_components.js
@@ -2,7 +2,7 @@ $.get("/components/navbar.html", html => {
     let tmp = $("<div>").html(html);
     let tpl = tmp.find("#navbar-template")[0];
     $("#navbar").append($(tpl.content).clone()); 
-window.dispatchEvent(new Event("navbarLoaded"));
+    window.dispatchEvent(new Event("navbarLoaded"));
 });
 
 $.get("/components/footer.html", html => {
