bbed82e50dda81df128d01edc137a8174533b872
UtterEvergreen1 <102389559+UtterEvergreen1@users.noreply.github.com>
2026-06-16 00:34:02 -0700

    Removed learn more.

--- assets/js/heros/display_heros.mjs
+++ assets/js/heros/display_heros.mjs
@@ -15,11 +15,6 @@ function getHero(title, desc, img_src, img_caption, index, p1, i1, p2, i2, p3, i
                 <li>${i2}${p2}</li>
                 <li>${i3}${p3}</li>
             </ul>
-            <a href="#">Learn More
-                <svg class="icon" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16">
-                    <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"></path>
-                </svg>
-            </a>
         </div>
     </div>
     <hr class="my-5" />
